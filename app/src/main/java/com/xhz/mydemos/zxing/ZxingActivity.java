14131adf227c7c0894ba54a5e96cb568ed2d1baa
sslyxhz <sslyxhz@hotmail.com>
2017-06-09 15:39:03 +0800

    Generate QRCode

--- app/src/main/java/com/xhz/mydemos/zxing/ZxingActivity.java
+++ app/src/main/java/com/xhz/mydemos/zxing/ZxingActivity.java
@@ -1,15 +1,18 @@
 package com.xhz.mydemos.zxing;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.xhz.mydemos.R;
 import com.xys.libzxing.zxing.activity.CaptureActivity;
+import com.xys.libzxing.zxing.encoding.EncodingUtils;
 
 /**
  * Created by xh.zeng on 2017/6/9.
@@ -18,7 +21,9 @@ import com.xys.libzxing.zxing.activity.CaptureActivity;
 public class ZxingActivity extends AppCompatActivity {
 
     private Button mBtnScanner;
+    private Button mBtnGenerate;
     private TextView mTvResult;
+    private ImageView mIvQrCode;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,18 +31,25 @@ public class ZxingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_zxing);
 
         initview();
-
     }
 
     private void initview() {
         mBtnScanner = (Button) findViewById(R.id.btn_scan);
+        mBtnGenerate = (Button) findViewById(R.id.btn_generate);
         mTvResult = (TextView) findViewById(R.id.tv_result);
+        mIvQrCode = (ImageView) findViewById(R.id.iv_qrcode);
 
         mBtnScanner.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivityForResult(new Intent(ZxingActivity.this , CaptureActivity.class) , 0);
-
+            }
+        });
+        mBtnGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bitmap qrCode = EncodingUtils.createQRCode("视讯，生成二维码",900,900,null);
+                mIvQrCode.setImageBitmap(qrCode);
             }
         });
 
